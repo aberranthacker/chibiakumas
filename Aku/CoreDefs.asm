6bd17f52cd7baae0bfc78dcce278b3af143a86a2
Oleg Tsymbalyuk <aberrant.hacker@gmail.com>
2019-01-02 20:26:37 +0300

    cleanup CoreDefs.asm

--- Aku/CoreDefs.asm
+++ Aku/CoreDefs.asm
@@ -1,328 +1,68 @@
-ifdef buildCPC
-    macro exxdi
-        di
-    mend
-    macro exxei
-        ei
-    mend
-    macro RstSix
-        rst 6
-    mend
+macro exxdi
+    di
+mend
+macro exxei
+    ei
+mend
+macro RstSix
+    rst 6
+mend
 
-    macro ldai
-        ld a,i
-    mend
-    macro ldia
-        ld i,a
-    mend
+macro ldai
+    ld a,i
+mend
+macro ldia
+    ld i,a
+mend
 
-    SPReset equ &BFFF
-    Akuyou_CoreStart      equ &450
-    Akuyou_BootStrapStart equ &8000
+SPReset equ &BFFF
+Akuyou_CoreStart      equ &450
+Akuyou_BootStrapStart equ &8000
 
-    Akuyou_GameVars    equ &3100 ; Need &700 bytes!
-    PlusSprite_ExtBank equ &C7
-    Akuyou_PlayerSpritePos equ &3800
-    Akuyou_PlusSpritesPos  equ &3800;&A800-&8000
-    ;Akuyou_PlusSpritesPosOneByte equ &38;&A800-&8000
-    ;Akuyou_PlusSpritesBoPosOneByte equ &F8
+Akuyou_GameVars    equ &3100 ; Need &700 bytes!
+PlusSprite_ExtBank equ &C7
+Akuyou_PlayerSpritePos equ &3800
+Akuyou_PlusSpritesPos  equ &3800;&A800-&8000
+;Akuyou_PlusSpritesPosOneByte equ &38;&A800-&8000
+;Akuyou_PlusSpritesBoPosOneByte equ &F8
 
-    Akuyou_Music_Bank  equ &C0
-    Akuyou_MusicPos    equ &50   ;Akuyou only allows &400 bytes for music
-    Akuyou_MusicPosAlt equ &7B00 ;When music switches during boss battle, store the alternate here
-    Akuyou_SfxPos      equ &3000 ;Akuyou only allows &100 bytes for SFX
+Akuyou_Music_Bank  equ &C0
+Akuyou_MusicPos    equ &50   ;Akuyou only allows &400 bytes for music
+Akuyou_MusicPosAlt equ &7B00 ;When music switches during boss battle, store the alternate here
+Akuyou_SfxPos      equ &3000 ;Akuyou only allows &100 bytes for SFX
 
-    Akuyou_LevelStart      equ &4000 ;+3-Bank 0
-    Akuyou_LevelStart_Bank equ &C0
+Akuyou_LevelStart      equ &4000 ;+3-Bank 0
+Akuyou_LevelStart_Bank equ &C0
 
-    LevelData128kpos      equ &4000  ;+3-Bank 1
-    LevelData128kpos_Bank equ &C7
+LevelData128kpos      equ &4000  ;+3-Bank 1
+LevelData128kpos_Bank equ &C7
 
-    LevelData128kpos_C      equ &4000 ;+3-Bank 1
-    LevelData128kpos_C_Bank equ &C4
+LevelData128kpos_C      equ &4000 ;+3-Bank 1
+LevelData128kpos_C_Bank equ &C4
 
-    LevelData128kpos_D         equ &6000 ;+3-Bank 1
-    LevelData128kpos_D_OneByte equ &60
-    LevelData128kpos_D_Bank    equ &C4
+LevelData128kpos_D         equ &6000 ;+3-Bank 1
+LevelData128kpos_D_OneByte equ &60
+LevelData128kpos_D_Bank    equ &C4
 
-    CSprite_SetDisk  equ 2*3
-    CSprite_Continue equ 1*3
-    CSprite_Loading  equ 0*3
+CSprite_SetDisk  equ 2*3
+CSprite_Continue equ 1*3
+CSprite_Loading  equ 0*3
 
-    Font_Membank        equ &C6
-    Font_RegularSizePos equ &7000;&F000
-    if BuildLang= ''
-        Font_SmallSizePos equ &7800;&F800
-    else
-        Font_SmallSizePos equ &7A00;&F800
-    endif
+Font_Membank        equ &C6
+Font_RegularSizePos equ &7000;&F000
+Font_SmallSizePos   equ &7800;&F800
 
 ;Platform Specific Core commands;
-    Akuyou_RasterColors_SetPointers equ Akuyou_CoreStart+&009C ;Set the memory location of the CPC raster switching data
-    Akuyou_BankSwitch_C0_BankCopy   equ Akuyou_CoreStart+&0066 ;Copy data between banks on CPC (banks numbered C0-C7 etc)
-    Akuyou_BankSwitch_C0            equ Akuyou_CoreStart+&003F ;Switch Banks for a moment (Disable interrupts first!)
-    Akuyou_BankSwitch_C0_Reset      equ Akuyou_CoreStart+&0042 ;Reset to bank that was made current
-    Akuyou_BankSwitch_C0_SetCurrent equ Akuyou_CoreStart+&00B7 ;set a bank as 'Current'
+Akuyou_RasterColors_SetPointers equ Akuyou_CoreStart+&009C ;Set the memory location of the CPC raster switching data
+Akuyou_BankSwitch_C0_BankCopy   equ Akuyou_CoreStart+&0066 ;Copy data between banks on CPC (banks numbered C0-C7 etc)
+Akuyou_BankSwitch_C0            equ Akuyou_CoreStart+&003F ;Switch Banks for a moment (Disable interrupts first!)
+Akuyou_BankSwitch_C0_Reset      equ Akuyou_CoreStart+&0042 ;Reset to bank that was made current
+Akuyou_BankSwitch_C0_SetCurrent equ Akuyou_CoreStart+&00B7 ;set a bank as 'Current'
 
-    Akuyou_RasterColors_Reset   equ Akuyou_CoreStart+&0030 ;Reset raster colors
-    Akuyou_RasterColors_Disable equ Akuyou_CoreStart+&0033 ;Disable raster flips
+Akuyou_RasterColors_Reset   equ Akuyou_CoreStart+&0030 ;Reset raster colors
+Akuyou_RasterColors_Disable equ Akuyou_CoreStart+&0033 ;Disable raster flips
 
-    Akuyou_CPCGPU_CommandNum equ Akuyou_CoreStart+&00AB ;Run a graphics command from the CPC Graphics vector array
-endif
-
-ifdef buildENT ; {{{
-    ;0=F8 (Zero Page)
-    ;1=F9
-    ;2=FA
-    ;3=FB
-    ;4=FC  (Screen1)
-    ;5=FD  (Screen2)
-    ;6=FE
-    ;7=FF (System page)
-
-    macro exxdi
-        di
-    mend
-    macro exxei
-        ei
-    mend
-    macro RstSix
-;       call Rst6Fake
-        rst 5
-    mend
-
-    macro ldai
-        ld a,i
-    mend
-    macro ldia
-        ld i,a
-    mend
-
-    SPReset equ &BFFF
-    Akuyou_CoreStart equ &500
-
-    Akuyou_BootStrap_Bank equ &FE
-    Akuyou_BootStrapStart equ &8000
-
-    Akuyou_GameVars equ &3100 ; Need &700 bytes!
-
-    PlusSprite_ExtBank equ &FB
-    Akuyou_PlusSpritesPos equ &4000
-    Akuyou_PlayerSpritePos equ &4000
-
-    Akuyou_ScreenBufferA equ &FD
-    Akuyou_ScreenBufferB equ &FC
-
-;   Akuyou_Music_Bank equ &F8
-    Akuyou_MusicPos equ &100        ;Akuyou only allows &400 bytes for music
-    Akuyou_MusicPosAlt equ &7B00        ;When music switches during boss battle, store the alternate here
-    Akuyou_SfxPos equ &3000     ;Akuyou only allows &100 bytes for SFX
-
-    Akuyou_LevelStart equ &4000     ;+3-Bank 0
-    Akuyou_LevelStart_Bank equ &F9
-
-    LevelData128kpos equ &4000      ;+3-Bank 1
-    LevelData128kpos_Bank equ &FA
-
-    LevelData128kpos_C equ &6000        ;+3-Bank 1
-    LevelData128kpos_C_OneByte equ &70
-    LevelData128kpos_C_Bank equ &FA
-
-    LevelData128kpos_D equ &7000        ;+3-Bank 1
-    LevelData128kpos_D_OneByte equ &70
-    LevelData128kpos_D_Bank equ &FA
-
-    CSprite_SetDisk equ 2*3
-    CSprite_Continue equ 1*3
-    CSprite_Loading equ 0*3
-
-    CSprite_Bank equ &FB
-    CSprite_Pos equ &6000
-    CSprite_PosByte equ &60
-
-    Font_Membank equ &FE
-    Font_RegularSizePos equ &7000;&F000
-    Font_SmallSizePos equ   &7800;&F800
-
-;Platform Specific Core commands;
-    Akuyou_RasterColors_SetPointers equ     Akuyou_CoreStart+&009C      ;Set the memory location of the CPC raster switching data
-    Akuyou_BankSwitch_C0_BankCopy equ   Akuyou_CoreStart+&0066      ;Copy data between banks on CPC (banks numbered C0-C7 etc)
-    Akuyou_BankSwitch_C0 equ        Akuyou_CoreStart+&003F      ;Switch Banks for a moment (Disable interrupts first!)
-    Akuyou_BankSwitch_C0_Reset equ  Akuyou_CoreStart+&0042          ;Reset to bank that was made current
-    Akuyou_BankSwitch_C0_SetCurrent  equ  Akuyou_CoreStart+&00B7        ;set a bank as 'Current'
-
-    Akuyou_RasterColors_Reset equ       Akuyou_CoreStart+&0030      ;Reset raster colors
-    Akuyou_RasterColors_Disable equ     Akuyou_CoreStart+&0033      ;Disable raster flips
-
-    Akuyou_ENTGPU_CommandNum        equ     Akuyou_CoreStart+&00AB  ;Run a graphics command from the CPC Graphics vector array
-endif ; }}}
-ifdef buildMSX ; {{{
-    macro exxdi
-        di
-    mend
-    macro exxei
-        ei
-    mend
-
-    macro RstSix
-        rst 6
-    mend
-
-    macro ldai
-        ld a,i
-    mend
-    macro ldia
-        ld i,a
-    mend
-
-    SPReset equ &D100
-
-    Akuyou_CoreStart equ &8000  ;&450+
-    Akuyou_BootStrapStart equ &1000
-
-    Akuyou_GameVars equ &C000-&700 ; Need &700 bytes!
-    PlusSprite_ExtBank equ &C7
-
-    Akuyou_PlayerSpritePos equ &100     ;Memory pos of Player sprite lookup (160 bytes)
-    Akuyou_PlusSpritesPos equ &200      ;Memory pos of Icon   sprite lookup (160 bytes)
-
-ifndef buildMSX_V9K
-    Akuyou_PlayerSpritePos_Y equ 192        ;Memory pos of Player sprite lookup (160 bytes)
-    Akuyou_PlusSpritesPos_Y  equ 1024-64        ;Memory pos of Icon   sprite lookup (160 bytes)
-    Akuyou_FontPos_Y    equ 1024-64+16
-    Akuyou_LevelSprites_Y   equ 256+192;512
-    Akuyou_V9K_Paralax_Y    equ 1024        ;For compatibility
-else
-    Akuyou_CompiledSprites_Y equ 3264
-    Akuyou_PlayerSpritePos_Y equ 3840       ;Memory pos of Player sprite lookup (160 bytes)
-    Akuyou_PlusSpritesPos_Y  equ 3840+64        ;Memory pos of Icon   sprite lookup (160 bytes)
-    Akuyou_FontPos_Y    equ 3840+64+16
-    Akuyou_LevelSprites_Y   equ 512
-    Akuyou_V9K_Paralax_Y    equ 1024
-    ParticleArrayPointer equ &3D00      ;FX star array - V9990 only!
-endif
-    Akuyou_Music_Bank equ 0
-    Akuyou_MusicPos    equ &C000;&50+&8000      ;Akuyou only allows &400 bytes for music
-    Akuyou_SfxPos      equ &C400 ;&3000+&8000   ;Akuyou only allows &100 bytes for SFX
-    Akuyou_MusicPosAlt equ &C500 ;-&C900        ;When music switches during boss battle, store the alternate here
-
-;   Akuyou_LevelStart equ &4000
-;   LevelData128kpos equ &C000;&D000    ;Note Reffed to C1
-
-    Akuyou_LevelStart equ &4000     ;+3-Bank 0
-;   Akuyou_LevelStart_FirstByte equ &40
-    Akuyou_LevelStart_Bank equ 0
-
-    LevelData128kpos equ &C00       ;+3-Bank 1
-;   LevelData128kpos_FirstByte equ &C
-    LevelData128kpos_Bank equ 1
-    ;Core is in bank 2
-    LevelData128kpos_C equ &400     ;+3-Bank 1
-;   LevelData128kpos_C_FirstByte equ &4
-    LevelData128kpos_C_Bank equ 3
-
-    LevelData128kpos_D equ &800     ;+3-Bank 1
-;   LevelData128kpos_D_FirstByte equ &8
-    LevelData128kpos_D_Bank equ 3
-
-    CSprite_SetDisk equ 2
-    CSprite_Continue equ 1
-    CSprite_Loading equ 0
-
-    Font_Membank equ &C6
-    Font_RegularSizePos equ &7000;&F000
-    Font_SmallSizePos equ   &7800;&F800
-
-;Platform Specific Core commands;
-    Akuyou_VDP_CommandNum           equ     Akuyou_CoreStart+&00AB  ;MSX VDP command Vector Array
-    Akuyou_VDP_SetPalette           equ     Akuyou_CoreStart+&009C  ;Set MSX palette
-    Akuyou_VDP_RLEProcessorFromMemory   equ     Akuyou_CoreStart+&00E7  ;Read an RLE bitmap from standard memory not disk
-    Akuyou_Background_Tiles         equ     Akuyou_CoreStart+&00B4  ;Draw MSX background bitmap tiles
-endif ; }}}
-ifdef buildZXS ; {{{
-    macro RstSix
-        call Rst6Fake
-    mend
-
-    macro ldai
-        ld a,(ldai_Plus1-1)
-    mend
-    macro ldia
-        ld (ldai_Plus1-1),a
-    mend
-
-    ldai_Plus1 equ StarArrayPointer-1
-
-    macro exxdi
-        ;Di should go here
-    mend
-    macro exxei
-        ;Ei should go here
-    mend
-
-    SPReset equ &8181
-
-    Akuyou_CoreStart equ &8000+&190;    defs &183   ; 387 bytes for interrupt handler (and stack!)
-    Akuyou_CoreStart_Bank equ 0
-    Akuyou_GameVars equ &8000-&700 ; Need &700 bytes!
-
-    Akuyou_BootStrapStart equ &E000     ;+3-Bank 6
-    Akuyou_BootStrapStart_Bank equ 6
-
-    PlusSprite_ExtBank equ &C7
-    Akuyou_PlayerSpritePos equ &6000;&3800+&8000
-    Akuyou_PlusSpritesPos equ &6000 ;&3800+&8000
-
-    Akuyou_MusicPos equ    &C000        ;Akuyou only allows &400 bytes for music
-    Akuyou_SfxPos equ      &C400 ;&3000+&8000       ;Akuyou only allows &100 bytes for SFX
-    Akuyou_MusicPosAlt equ &C500        ;When music switches during boss battle, store the alternate here
-
-    Akuyou_Music_Bank equ 6
-
-    Akuyou_LevelStart equ &C000     ;+3-Bank 0
-    Akuyou_LevelStart_Bank equ 0
-
-    LevelData128kpos equ &C000      ;+3-Bank 1
-    LevelData128kpos_Bank equ 1
-    ;Core is in bank 2
-    LevelData128kpos_C equ &C000        ;+3-Bank 1
-    LevelData128kpos_C_Bank equ 3
-
-    LevelData128kpos_D equ &E000        ;+3-Bank 1
-    LevelData128kpos_D_Bank equ 3
-
-    CSprite_SetDisk equ 6
-    CSprite_Continue equ 12
-    CSprite_Loading equ 0
-
-    Font_Membank equ 4
-    Font_RegularSizePos equ &C000;&F000
-    Font_SmallSizePos equ   &C400;&F800
-
-    CompiledSprite_Membank equ 4
-    CompiledSprite_Pos equ &E000;&F000
-
-    ;Platform Specific Core commands;
-    Akuyou_Bankswapper_Copy          equ    Akuyou_CoreStart+&0066      ;Copy a chunk of memory from one bank to another (ZX bank 0-7)
-
-    Akuyou_Background_Color      equ    Akuyou_CoreStart+&009C      ;Color the background
-    Akuyou_BackgroundFloodFillQuadSprite equ Akuyou_CoreStart+&00AB     ;Tile fill an area of background
-    Akuyou_BackgroundSolidFill       equ Akuyou_CoreStart+&00AE     ;Solid fill an area of background
-
-    Akuyou_GetSpriteMempos           equ Akuyou_CoreStart+&00B1     ;Get sprite memory position (used for tiled backgrounds)
-    Akuyou_BitShifter            equ Akuyou_CoreStart+&00E7     ;Bitshift background tiles
-    Akuyou_INT_Force             equ Akuyou_CoreStart+&00B4     ;Force an interrupt tick
-
-    Akuyou_Bankswapper_CallDefined_Set  equ Akuyou_CoreStart+&003F  ;Define a memory location and a bank we will call later
-    Akuyou_Bankswapper_CallDefined      equ Akuyou_CoreStart+&0042  ;call defined... this allows us to pass all registers
-    Akuyou_Bankswapper_Set          equ Akuyou_CoreStart+&00BD  ;Set a temporary memory bank (0-7)
-    Akuyou_Bankswapper_Reset        equ Akuyou_CoreStart+&00C0  ;Restore the previous current memory bank
-
-
-    Akuyou_Rle_FromPointers     equ Akuyou_CoreStart+&0030
-
-    Akuyou_ZXSGPU_CommandNum    equ Akuyou_CoreStart+&0033
-endif ; }}}
+Akuyou_CPCGPU_CommandNum equ Akuyou_CoreStart+&00AB ;Run a graphics command from the CPC Graphics vector array
 
 ;Core Jumpblock definitions
 
@@ -458,6 +198,7 @@ Keymap_Pause equ 7
 Keymap_AnyFire equ %11001111
 
 org Akuyou_GameVars ; Need &700 bytes!
+
 StarArraySize       equ 255
 ObjectArraySize     equ 60 ; Must be under 64!
 PlayerStarArraySize equ 128
