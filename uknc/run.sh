f086b13c28761cd89b00aeb01f7c620a665c634d
Oleg Tsymbalyuk <aberrant.hacker@gmail.com>
2021-07-25 05:09:42 +0300

    minor changes

--- uknc/run.sh
+++ uknc/run.sh
@@ -3,6 +3,7 @@
 make
 
 echo starting UKNCBTL
-wine cmd.exe /c "z:\home\random\opt\UKNCBTL\UKNCBTL.exe /boot" 2>/dev/null
+#wine cmd.exe /c "z:\home\random\opt\UKNCBTL\UKNCBTL.exe /boot" 2>/dev/null
+~/opt/QtUkncBtl/QtUkncBtl -boot1
 
 echo done
