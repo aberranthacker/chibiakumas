36c066c2d72b66c41245091ce283ff7f14b5ea17
Oleg Tsymbalyuk <aberrant.hacker@gmail.com>
2022-05-30 22:46:48 +0300

    cleanup

--- uknc/virtual_screen_pos_320.s
+++ uknc/virtual_screen_pos_320.s
@@ -22,11 +22,7 @@ from the list
          R2 Y lines to skip;  R3 Y lines to remove
 --------------------------------------------------------------------------------
 */
-VirtualPosToScreenByte:                                     # VirtualPosToScreenByte:
-                                                            #     ld HL,&0000
-                                                            #     ld D,h
-                                                            #     ld e,h
-
+VirtualPosToScreenByte:
         #  we use a virtual screen size
         #  X width  is 208 (160 onscreen - 2 per byte)
         #  Y height is 248 (200 onscreen - 1 per line)
@@ -38,147 +34,158 @@ VirtualPosToScreenByte:                                     # VirtualPosToScreen
 
         # R1 = Y, R2 = X, R0 = Xoff
     #---------------------------------------------------------------------------
+    .list
         CLR  R4 # X bytes to skip
+    .nolist
         CLR  R5 # X bytes to remove
-                                                            #     ; only works with 24 pixel sprites
-        # check X                                           #     ld a,B  ;Check X
-        CMP  R2,(PC)+; cmpSpriteSizeConfig24: .word 24      #     cp 24 :SpriteSizeConfig24_Plus1
-        BHIS VirtualPos_1$                                  #     jp NC,VirtualPos_1  ; jp is faster if we expect it to be true!
-        # X < 24                                            # ; X<24
-        MOV  (PC)+,R4; srcSpriteSizeConfig25: .word 25      #     ld a,25 :SpriteSizeConfig25_Plus1
-        SUB  R2,R4                                          #     sub a,B
-        RORB R4                                             #     RRA
-        MOV  R4,R5                                          #     ld H,A  ;move the sprite A left
-                                                            #     ld L,A  ;need to plot A less bytes
-                                                            #
-        MOV  (PC)+,R2; srcSpriteSizeConfig24B: .word 24     #     ld B,24  :SpriteSizeConfig24B_Plus1;B was offscreen, so move it back on
-        BR   VirtualPos_2$                                  #     jp VirtualPos_2
-    VirtualPos_1$:                                          # VirtualPos_1:
-                                                            #     ;ld a,B ;Check X
-        CMP  R2,(PC)+; cmpSpriteSizeConfig184less12: .word 184-12 #     cp 184-12 :SpriteSizeConfig184less12_Plus1
-        BLOS VirtualPos_2$ # X <= 172                       #     jp C,VirtualPos_1
+        # only works with 24 pixel sprites
+        CMP  R2,$24     # check X
+       .equiv cmpSpriteSizeConfig24, .-2
+        BHIS VirtualPos_1$
+        # X < 24
+        MOV  $25,R4
+       .equiv srcSpriteSizeConfig25, .-2
+        SUB  R2,R4
+        RORB R4     # move the sprite R4 left
+        MOV  R4,R5  # need to plot R4 less bytes
+        MOV  $24,R2 # R2 was offscreen, so move it back on
+       .equiv srcSpriteSizeConfig24B, .-2
+        BR   VirtualPos_2$
+
+    VirtualPos_1$:
+        CMP  R2,$184-12 # check X
+       .equiv cmpSpriteSizeConfig184less12, .-2
+        BLOS VirtualPos_2$ # X <= 172
         # X > 172
-        MOV  R2,R0                                          #     ld a,B
-        SUB  (PC)+,R0; srcSpriteSizeConfig184less12: .word 184-12 #     sub 184-12 :SpriteSizeConfig184less12B_Plus1
-                                                            #
-        RORB R0                                             #     RRA
-                                                            #
-        ADD  R0,R5 # X pos is ok, but plot R0 less -words-  #     add L   ;   X pos is ok, but plot A less bytes
-                                                            #     ld L,A
-                                                            #     ;ld a,B ;Check X
-    VirtualPos_2$:                                          # VirtualPos_2:
-                                                            #     ld a,B  ;Check X
-        SUB  (PC)+,R2; srcSpriteSizeConfig24C: .word 24     #     sub 24 :SpriteSizeConfig24C_Plus1
-        RORB R2                                             #     RRA ; halve the result, as we have 80 bytes, but 160 x co-ords
-        MOVB R2,@$srcSprShow_TempX                          #     ld B,a
+        MOV  R2,R0
+        SUB  $184-12,R0
+       .equiv srcSpriteSizeConfig184less12, .-2
+        RORB R0
+        ADD  R0,R5 # X pos is ok, but plot R0 less -bytes-
+
+    VirtualPos_2$:
+        SUB  $24,R2
+       .equiv srcSpriteSizeConfig24C, .-2
+        RORB R2 # halve the result, as we have 80 bytes, but 160 x co-ords
+        # using MOVB because MSB contains frame buffer offset
+        # show_sprite.s:265
+        MOVB R2,@$srcSprShow_TempX
     #---------------------------------------------------------------------------
         # R1 Y
         CLR  R2 # Y lines to skip
         CLR  R3 # Y lines to remove
-                                                            #     ld a,C  ;Check Y
-        CMP  R1,(PC)+; cmpSpriteSizeConfig24D: .word 24     #     cp 24 :SpriteSizeConfig24D_Plus1
-        BHIS VirtualPos_3$                                  #     jp NC,VirtualPos_3
-                                                            # ; Y<24
-        MOV  (PC)+,R2; srcSpriteSizeConfig24E: .word 24     #     ld a,24 :SpriteSizeConfig24E_Plus1
-        SUB  R1,R2                                          #     sub a,C
-        MOV  R2,R3                                          #     ld D,A  ;move the sprite A up
-                                                            #     ld E,A  ;need to plot A less lines
-        MOV  (PC)+,R1; srcSpriteSizeConfig24F: .word 24     #     ld C,24 :SpriteSizeConfig24F_Plus1
-        BR   VirtualPos_4$                                  #     jp VirtualPos_4
-    VirtualPos_3$:                                          # VirtualPos_3:
-                                                            #     ;ld a,C ;Check Y
-        CMP  R1,(PC)+; cmpSpriteSizeConfig224less24: .word 224-24 #     cp 224-24 :SpriteSizeConfig224less24_Plus1
-        BLO  VirtualPos_4$                                  #     jp C,VirtualPos_4
-                                                            # ; Y>224
-        MOV  R1,R3                                          #     ld a,C
-        SUB  (PC)+,R3; srcSpriteSizeConfig224less24: .word 224-24 #     sub 224-24 :SpriteSizeConfig224less24B_Plus1
-                                                            #     ld E,A
-    VirtualPos_4$:                                          # VirtualPos_4:
-        SUB  (PC)+,R1; srcSpriteSizeConfig24G: .word 24     #     ld a,C  ;Check Y
-        MOV  R1,@$srcSprShow_TempY                          #     sub 24 :SpriteSizeConfig24G_Plus1
-                                                            #     ld C,a
-RETURN                                                      #     ret
 
-ShowSpriteReconfigureEnableDisable:                         # ShowSpriteReconfigureEnableDisable:
+        CMP  R1,$24 # check Y
+       .equiv cmpSpriteSizeConfig24D, .-2
+        BHIS VirtualPos_3$
+        # Y < 24
+        MOV  $24,R2
+       .equiv srcSpriteSizeConfig24E, .-2
+        SUB  R1,R2 # move the sprite R2  up
+        MOV  R2,R3 # need to plot A less lines
+        MOV  $24,R1
+       .equiv srcSpriteSizeConfig24F, .-2
+        BR   VirtualPos_4$
+
+    VirtualPos_3$:
+        CMP  R1,$224-24 # check Y
+       .equiv cmpSpriteSizeConfig224less24, .-2
+        BLO  VirtualPos_4$
+        # Y > 224
+        MOV  R1,R3
+        SUB  $224-24,R3
+       .equiv srcSpriteSizeConfig224less24, .-2
+
+    VirtualPos_4$:
+        SUB  $24,R1
+       .equiv srcSpriteSizeConfig24G, .-2
+        MOV  R1,@$srcSprShow_TempY
+
+        RETURN
+
+#-------------------------------------------------------------------------------
        .global ShowSpriteReconfigureEnableDisable
-        MOV  $null,R5                                       #     ld hl,null
-        TST  R0                                             #     or a
-        BEQ  ShowSpriteReconfigureEnableDisableB            #     jr z,ShowSpriteReconfigureEnableDisableB
-        MOV  $ShowSpriteReconfigure,R5                      #     ld hl,ShowSpriteReconfigure
-ShowSpriteReconfigureEnableDisableB:                        # ShowSpriteReconfigureEnableDisableB:
-        MOV  R5,@$dstShowSpriteReconfigureCommand           #     ld (ShowSpriteReconfigureCommand_Plus2-2),hl
-        BR   ShowSpriteReconfigure_24px                     #     jr ShowSpriteReconfigure_24px
+ShowSpriteReconfigureEnableDisable:
+        MOV  $null,R5
+        TST  R0
+        BEQ  ShowSpriteReconfigureEnableDisableB
+        MOV  $ShowSpriteReconfigure,R5
+ShowSpriteReconfigureEnableDisableB:
+        MOV  R5,@$dstShowSpriteReconfigureCommand
+        BR   ShowSpriteReconfigure_24px
 
-ShowSpriteReconfigure:                                      # ShowSpriteReconfigure:
-        MOV  R1,@$cmpSpriteSizeConfig6                      #     ld (SpriteSizeConfig6_Plus1-1),a
+ShowSpriteReconfigure:
+        MOV  R1,@$cmpSpriteSizeConfig6
         # Akuyou was designed for 24x24 sprites, but this module can
         # 'reconfigure' it for other sizes
-        CMP  R1,$6                                          #     cp 6 ;24px
-        BEQ  ShowSpriteReconfigure_24px                     #     jp z,ShowSpriteReconfigure_24px
-        CMP  R1,$8                                          #     cp 8 ;32px
-        BEQ  ShowSpriteReconfigure_32px                     #     jp z,ShowSpriteReconfigure_32px
-        CMP  R1,$12                                         #     cp 12 ;32px
-        BEQ  ShowSpriteReconfigure_48px                     #     jp z,ShowSpriteReconfigure_48px
-        CMP  R1,$16                                         #     cp 16 ;64px
-        BEQ  ShowSpriteReconfigure_64px                     #     jr z,ShowSpriteReconfigure_64px
-        CMP  R1,$20                                         #     cp 20 ;80px
-        BEQ  ShowSpriteReconfigure_80px                     #     jr z,ShowSpriteReconfigure_80px
-        CMP  R1,$24                                         #     cp 24 ;96px
-        BEQ  ShowSpriteReconfigure_96px                     #     jr z,ShowSpriteReconfigure_96px
-        CMP  R1,$32                                         #     cp 32 ;128px
-        BEQ  ShowSpriteReconfigure_128px                    #     jr z,ShowSpriteReconfigure_128px
-        CMP  R1,$2                                          #     cp 2 ;8px
-        BEQ  ShowSpriteReconfigure_8px                      #     jr z,ShowSpriteReconfigure_8px
-        CMP  R1,$4                                          #     cp 4 ;16px
-        BEQ  ShowSpriteReconfigure_16px                     #     jr z,ShowSpriteReconfigure_16px
-RETURN                                                      # ret
-                                                            #
-ShowSpriteReconfigure_128px: # Not actually used!           # ShowSpriteReconfigure_128px:     ;Not actually used!
-        MOV  $184-64,R1                                     #     ld a,184-64
-        MOV  $224-24,R2                                     #     ld b,224-24
-        BR   ShowSpriteReconfigure_all                      #     jr ShowSpriteReconfigure_all
-ShowSpriteReconfigure_104px:                                # ShowSpriteReconfigure_104px:     ;Not actually used!
-        MOV  $184-52,R1                                     #     ld a,184-52
-        MOV  $224-96,R2                                     #     ld b,224-96
-        BR   ShowSpriteReconfigure_all                      #     jr ShowSpriteReconfigure_all
-ShowSpriteReconfigure_96px:                                 # ShowSpriteReconfigure_96px:      ;Used by Boss 1
-        MOV  $184-48,R1                                     #     ld a,184-48
-        MOV  $224-24,R2                                     #     ld b,224-24
-        BR   ShowSpriteReconfigure_all                      #     jr ShowSpriteReconfigure_all
-ShowSpriteReconfigure_80px:                                 # ShowSpriteReconfigure_80px:      ;Not actually used!
-        MOV  $184-40,R1                                     #     ld a,184-40
-        MOV  $224-80,R2                                     #     ld b,224-80
-        BR   ShowSpriteReconfigure_all                      #     jr ShowSpriteReconfigure_all
-ShowSpriteReconfigure_48px:                                 # ShowSpriteReconfigure_48px:
-        MOV  $184-24,R1                                     #     ld a,184-24
-        MOV  $224-48,R2                                     #     ld b,224-48
-        BR   ShowSpriteReconfigure_all                      #     jr ShowSpriteReconfigure_all
-ShowSpriteReconfigure_32px:                                 # ShowSpriteReconfigure_32px:
-        MOV  $184-16,R1                                     #     ld a,184-16
-        MOV  $224-32,R2                                     #     ld b,224-32
-        BR   ShowSpriteReconfigure_all                      #     jr ShowSpriteReconfigure_all
-ShowSpriteReconfigure_64px:                                 # ShowSpriteReconfigure_64px:      ;not actually used
-        MOV  $184-32,R1                                     #     ld a,184-32
-        MOV  $224-64,R2                                     #     ld b,224-64
-        BR   ShowSpriteReconfigure_all                      #     jr ShowSpriteReconfigure_all
-ShowSpriteReconfigure_16px:                                 # ShowSpriteReconfigure_16px:
-        MOV  $184- 8,R1                                     #     ld a,184-8
-        MOV  $224-16,R2                                     #     ld b,224-16
-        BR   ShowSpriteReconfigure_all                      #     jr ShowSpriteReconfigure_all
-ShowSpriteReconfigure_8px:                                  # ShowSpriteReconfigure_8px:
-        MOV  $184- 4,R1                                     #     ld a,184-4
-        MOV  $224- 8,R2                                     #     ld b,224-8
-        BR   ShowSpriteReconfigure_all                      #     jr ShowSpriteReconfigure_all
-ShowSpriteReconfigure_24px:                                 # ShowSpriteReconfigure_24px:
-        MOV  $184-12,R1                                     #     ld a,184-12
-        MOV  $224-24,R2                                     #     ld b,224-24
-                                                            #
-ShowSpriteReconfigure_all:                                  # ShowSpriteReconfigure_all:
-        # Right X                                           #     ;Right X
-        MOV  R1,$cmpSpriteSizeConfig184less12               #     ld (SpriteSizeConfig184less12_Plus1-1),a
-        MOV  R1,$srcSpriteSizeConfig184less12               #     ld (SpriteSizeConfig184less12B_Plus1-1),a
-                                                            #     ld a,B
-        # Bottom Y                                          #     ;Bottom Y
-        MOV  R2,$cmpSpriteSizeConfig224less24               #     ld (SpriteSizeConfig224less24_Plus1-1),a
-        MOV  R2,$srcSpriteSizeConfig224less24               #     ld (SpriteSizeConfig224less24B_Plus1-1),a
-RETURN                                                      # ret
+        CMP  R1,$6
+        BEQ  ShowSpriteReconfigure_24px
+        CMP  R1,$8
+        BEQ  ShowSpriteReconfigure_32px
+        CMP  R1,$12
+        BEQ  ShowSpriteReconfigure_48px
+        CMP  R1,$16
+        BEQ  ShowSpriteReconfigure_64px
+        CMP  R1,$20
+        BEQ  ShowSpriteReconfigure_80px
+        CMP  R1,$24
+        BEQ  ShowSpriteReconfigure_96px
+        CMP  R1,$32
+        BEQ  ShowSpriteReconfigure_128px
+        CMP  R1,$2
+        BEQ  ShowSpriteReconfigure_8px
+        CMP  R1,$4
+        BEQ  ShowSpriteReconfigure_16px
+
+        RETURN
+
+ShowSpriteReconfigure_128px: # Not actually used!
+        MOV  $184-64,R1
+        MOV  $224-24,R2
+        BR   ShowSpriteReconfigure_all
+ShowSpriteReconfigure_104px: # Not actually used!
+        MOV  $184-52,R1
+        MOV  $224-96,R2
+        BR   ShowSpriteReconfigure_all
+ShowSpriteReconfigure_96px:  # Used by Boss 1
+        MOV  $184-48,R1
+        MOV  $224-24,R2
+        BR   ShowSpriteReconfigure_all
+ShowSpriteReconfigure_80px:  # Not actually used!
+        MOV  $184-40,R1
+        MOV  $224-80,R2
+        BR   ShowSpriteReconfigure_all
+ShowSpriteReconfigure_48px:
+        MOV  $184-24,R1
+        MOV  $224-48,R2
+        BR   ShowSpriteReconfigure_all
+ShowSpriteReconfigure_32px:
+        MOV  $184-16,R1
+        MOV  $224-32,R2
+        BR   ShowSpriteReconfigure_all
+ShowSpriteReconfigure_64px:  # not actually used
+        MOV  $184-32,R1
+        MOV  $224-64,R2
+        BR   ShowSpriteReconfigure_all
+ShowSpriteReconfigure_16px:
+        MOV  $184- 8,R1
+        MOV  $224-16,R2
+        BR   ShowSpriteReconfigure_all
+ShowSpriteReconfigure_8px:
+        MOV  $184- 4,R1
+        MOV  $224- 8,R2
+        BR   ShowSpriteReconfigure_all
+ShowSpriteReconfigure_24px:
+        MOV  $184-12,R1
+        MOV  $224-24,R2
+
+ShowSpriteReconfigure_all:
+        # Right X
+        MOV  R1,$cmpSpriteSizeConfig184less12
+        MOV  R1,$srcSpriteSizeConfig184less12
+
+        # Bottom Y
+        MOV  R2,$cmpSpriteSizeConfig224less24
+        MOV  R2,$srcSpriteSizeConfig224less24
+
+        RETURN
