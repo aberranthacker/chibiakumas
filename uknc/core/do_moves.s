0098f65632fa92f52b31157bf96af372848fa74c
Oleg Tsymbalyuk <aberrant.hacker@gmail.com>
2022-08-29 12:15:19 +0300

    fix wave move pattern

--- uknc/core/do_moves.s
+++ uknc/core/do_moves.s
@@ -129,46 +129,37 @@ DoMoves_Spec: # Special moves - various kinds
       # R4=X, R1=Y, R2: LSB=move, MSB=anything
 DoMoves_Wave:
       # wave pattern  1010DSPP D Depth bit, S Speed, PP Position
-        MOV  R4,R1                  #     ld a,b
-        BIT  $0b1000,R2             #     bit 3,d
-        BNZ  DoMoves_Wave_TwoShifts #     jr nz,DoMoves_TwoShifts
-        ASLB R1                     #     srl a
-        ASLB R1                     #     srl a
-DoMoves_Wave_TwoShifts:
-                                    #     ;srl a  ; unrem for speedup
-        BIC  $0xFFE0,R1             #     and %00011111
-        CMP  R1,$0x10               #     cp  %00010000
-        BLO  DoMoves_Wave_Continue  #     jr C,DoMoves_WaveContinue
+        MOV  R4,R1
+        BIT  $0b1000,R2
+        BNZ  DoMoves_Wave_TwoShifts
+        ASR  R1
+        ASR  R1
+  DoMoves_Wave_TwoShifts:
+       #ASR  R1 # unrem for speedup
+        BIC  $0xFFE0,R1
+        CMP  R1,$0x10
+        BLO  DoMoves_Wave_Continue
 
-        MOV  $0x1F,R0
-        XOR  R0,R1                  #     xor %00011111
-DoMoves_Wave_Continue:              # DoMoves_WaveContinue:
-        BIT  $0b100,R2              #     bit 2,d
-        BNZ  DoMoves_Wave_SlowSpeed #     jr nz,DoMoves_WaveSlowSpeed
+        MOV  $0x001F,R0
+        XOR  R0,R1
+  DoMoves_Wave_Continue:
+        BIT  $0b100,R2
+        BNZ  DoMoves_Wave_SlowSpeed
 
-        ASLB R1                     #     sll a
-                                    #
-DoMoves_Wave_SlowSpeed:             # DoMoves_WaveSlowSpeed
-        ASLB R1                     #     sll a
-        ASLB R1                     #     sll a ; rem to reduce wave depth
-                                    #
-                                    #     ld C,a
-        MOV  R2,R0                  #     ld a,d
-        BIC  $0xFFFC,R0             #     and %00000011
-        ASH  $5,R0                  #     rrca
-                                    #     rrca
-                                    #     rrca
-                                    #     rrca  ; equivalent to 5 left shifts
-        BIS  $0b00011100,R0         #     or %00011100
-                                    #
-        ADD  R0,R1                  #     add C
-                                    #     ld C,a
-                                    #
-        DEC  R4                     #     dec B
-                                    #     ld a,b
-        CMP  R4,$24                 #     cp 24 ; we are at the bottom of the screen
-        BLO  DoMoves_Kill           #     jr C,DoMoves_Kill ; over the page
-        RETURN                      #     ret
+        ASLB R1
+  DoMoves_Wave_SlowSpeed:
+        ASLB R1
+        ASLB R1 # rem to reduce wave depth
+        MOV  R2,R0
+        BIC  $0xFFFC,R0
+        ASH  $5,R0
+        BIS  $0b00011100,R0
+        ADD  R0,R1
+        DEC  R4
+        CMP  R4,$24       # we are at the bottom of the screen
+        BLO  DoMoves_Kill # over the page
+
+        RETURN
 
 DoMoves_SeekerAuto:
                                         # push bc
