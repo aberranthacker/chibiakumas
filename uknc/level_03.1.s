9f5a7db10074e845cc52b1bd9004e4aba83aa36e
Oleg Tsymbalyuk <aberrant.hacker@gmail.com>
2022-09-21 22:56:36 +0300

    add mask for the rest of the level 03 sprites

--- uknc/level_03.1.s
+++ uknc/level_03.1.s
@@ -35,19 +35,19 @@
 
    # sprite 4 GrassTuftA
     .word GrassTuftA # sprite offset
-    .word 0 # bit-mask offset
+    .word GrassTuftAMask # bit-mask offset
     .byte 6 # height of the sprite
     .byte 0 # Y offset
     .byte 6 # witdh
-    .byte SPR_PSET # attributes
+    .byte SPR_HAS_MASK # attributes
 
    # sprite 5 GrassTuftB
     .word GrassTuftB # sprite offset
-    .word 0 # bit-mask offset
+    .word GrassTuftBMask # bit-mask offset
     .byte 6 # height of the sprite
     .byte 0 # Y offset
     .byte 6 # witdh
-    .byte SPR_PSET # attributes
+    .byte SPR_HAS_MASK # attributes
 
    # sprite 6 GrassTuftBig
     .word GrassTuftBig # sprite offset
@@ -59,19 +59,19 @@
 
    # sprite 7 GrassTuftC
     .word GrassTuftC # sprite offset
-    .word 0 # bit-mask offset
+    .word GrassTuftCMask # bit-mask offset
     .byte 6 # height of the sprite
     .byte 0 # Y offset
     .byte 6 # witdh
-    .byte SPR_PSET # attributes
+    .byte SPR_HAS_MASK # attributes
 
    # sprite 8 GrassTuftD
     .word GrassTuftD # sprite offset
-    .word 0 # bit-mask offset
+    .word GrassTuftDMask # bit-mask offset
     .byte 5 # height of the sprite
     .byte 0 # Y offset
     .byte 6 # witdh
-    .byte SPR_PSET # attributes
+    .byte SPR_HAS_MASK # attributes
 
    # sprite 9 Kamisagi
     .word Kamisagi # sprite offset
@@ -83,35 +83,35 @@
 
    # sprite 10 MukadeBachi1
     .word MukadeBachi1 # sprite offset
-    .word 0 # bit-mask offset
+    .word MukadeBachi1Mask # bit-mask offset
     .byte 14 # height of the sprite
     .byte 0 # Y offset
     .byte 6 # witdh
-    .byte SPR_PSET # attributes
+    .byte SPR_HAS_MASK # attributes
 
    # sprite 11 MukadeBachi2
     .word MukadeBachi2 # sprite offset
-    .word 0 # bit-mask offset
+    .word MukadeBachi2Mask # bit-mask offset
     .byte 24 # height of the sprite
     .byte 0 # Y offset
     .byte 6 # witdh
-    .byte SPR_PSET # attributes
+    .byte SPR_HAS_MASK # attributes
 
    # sprite 12 MukadeBachi3
     .word MukadeBachi3 # sprite offset
-    .word 0 # bit-mask offset
+    .word MukadeBachi3Mask # bit-mask offset
     .byte 12 # height of the sprite
     .byte 0 # Y offset
     .byte 6 # witdh
-    .byte SPR_PSET # attributes
+    .byte SPR_HAS_MASK # attributes
 
    # sprite 13 MukadeBachi5
     .word MukadeBachi5 # sprite offset
-    .word 0 # bit-mask offset
+    .word MukadeBachi5Mask # bit-mask offset
     .byte 13 # height of the sprite
     .byte 0 # Y offset
     .byte 6 # witdh
-    .byte SPR_PSET # attributes
+    .byte SPR_HAS_MASK # attributes
 
    # sprite 14 PowerupDrone
     .word PowerupDrone # sprite offset
@@ -178,9 +178,13 @@ GnatPackMask:
 
 GrassTuftA:
     .incbin "build/level_03/grass_tuft_a.1.bin"
+GrassTuftAMask:
+    .incbin "build/level_03/grass_tuft_a_mask.1.bin"
 
 GrassTuftB:
     .incbin "build/level_03/grass_tuft_b.1.bin"
+GrassTuftBMask:
+    .incbin "build/level_03/grass_tuft_b_mask.1.bin"
 
 GrassTuftBig:
     .incbin "build/level_03/grass_tuft_big.1.bin"
@@ -189,9 +193,13 @@ GrassTuftBigMask:
 
 GrassTuftC:
     .incbin "build/level_03/grass_tuft_c.1.bin"
+GrassTuftCMask:
+    .incbin "build/level_03/grass_tuft_c_mask.1.bin"
 
 GrassTuftD:
     .incbin "build/level_03/grass_tuft_d.1.bin"
+GrassTuftDMask:
+    .incbin "build/level_03/grass_tuft_d_mask.1.bin"
 
 Kamisagi:
     .incbin "build/level_03/kamisagi.1.bin"
@@ -200,15 +208,23 @@ KamisagiMask:
 
 MukadeBachi1:
     .incbin "build/level_03/mukade_bachi_1.1.bin"
+MukadeBachi1Mask:
+    .incbin "build/level_03/mukade_bachi_1_mask.1.bin"
 
 MukadeBachi2:
     .incbin "build/level_03/mukade_bachi_2.1.bin"
+MukadeBachi2Mask:
+    .incbin "build/level_03/mukade_bachi_2_mask.1.bin"
 
 MukadeBachi3:
     .incbin "build/level_03/mukade_bachi_3.1.bin"
+MukadeBachi3Mask:
+    .incbin "build/level_03/mukade_bachi_3_mask.1.bin"
 
 MukadeBachi5:
     .incbin "build/level_03/mukade_bachi_5.1.bin"
+MukadeBachi5Mask:
+    .incbin "build/level_03/mukade_bachi_5_mask.1.bin"
 
 PowerupDrone:
     .incbin "build/level_03/powerup_drone.1.bin"
