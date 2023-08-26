e7beb7fa4d2fd7c41c62df8da1a896bb54fea2fe
Oleg Tsymbalyuk <aberrant.hacker@gmail.com>
2023-08-26 22:57:51 +0300

    move build_tools to a submodule

--- uknc/build_tools/xor_text.rb
+++
@@ -1,9 +0,0 @@
-timer_ticks_occured = 0
-
-256.times do |timer_current_tick|
-  timer_ticks_occured = ((timer_current_tick + 1) ^ timer_current_tick) & 0xFF
-  print "#{timer_current_tick.to_s(2).rjust(8, '0')} "
-  print "#{timer_ticks_occured.to_s(2).rjust(8, '0')} "
-  print "#{(timer_ticks_occured & 0x10).to_s(2).rjust(8, '0')} "
-  puts
-end
